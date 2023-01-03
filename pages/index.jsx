f7fa5ef2a7c7ac940544a0436d4c5b04f717d94c
unknown <alencar.wesley@escola.pr.gov.br>
2023-01-03 17:14:16 -0300

    Atualizacion

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import banner from  '../public/banner.png'
 import events from '../public/events.png'
+import guia from '../public/guia.png'
+import recruit from '../public/recruit.webp'
 import Link from 'next/link'
 
 function Home() {
@@ -13,8 +15,16 @@ function Home() {
         <div className="lateral">
           <Link href="calendario">
             <div className="card">
-              <Image src={events} alt="Banner"/>
+              <Image src={events} alt="Calendario"/>
               <p>Calendario</p>
+            </div>
+            <div className="card">
+              <Image src={recruit} alt="Reclutamiento"/>
+              <p>Reclutamiento</p>
+            </div>
+            <div className="card">
+              <Image src={guia} alt="Guia"/>
+              <p>Guia</p>
             </div>
           </Link>
         </div>
